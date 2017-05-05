b19b035e5ce0af99bc84459a6d251ce5666a4565
Kriti Jain <jkriti269@gmail.com>
2017-05-05 23:33:08 +0530

    Add files via upload

---
+++ form/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+		
+class Member(models.Model):
+
+       
+       residing_state = models.CharField(max_length=50)
+       residing_city = models.CharField(max_length=50)
+
+
+
+class State(models.Model):
+
+         state=models.CharField(max_length=20)
+                
+
+class City(models.Model):
+
+        city=models.CharField(max_length=20)
+        state=models.ForeignKey(State)
---
+++ readme.txt
@@ -0,0 +1,17 @@
+Use Python 3.6 or Higher and set it in path, Using Windows 10 x-64 Version
+Open Command Prompt
+mkdir myadvo
+cd myadvo
+python -m venv myvenv
+myvenv\Scripts\activate
+pip install --upgrade pip
+pip install django
+pip install selenium
+Install GeckoDriver for Firefox https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-win64.zip version v0.16.1
+Extract the geckodriver.exe and paste in myadvo\myvenv\selenium\webdriver\firefox 
+django-admin.py startproject mysite .
+Extract the zip file downloaded from https://github.com/masterpranav/myadvo in myadvo folder.
+
+In CMD Type
+python manage.py runserver
+Open 127.0.0.1:8000 in your Browser
